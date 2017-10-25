69f06f1ae421ff47e7e94d5df49d480cbd5ee3b8
JaylonQi@outlook.com <JaylonQi@outlook.com>
2017-10-25 21:48:24 +0800

    fixed test

--- src/stories/screens/Home/index.tsx
+++ src/stories/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { View } from "react-native";
-import Swiper from "react-native-swiper";
+// import Swiper from "react-native-swiper";
 import { Container, Header, Title, Content, Text, Button, Icon, Left, Body, Right, List, ListItem, Footer } from "native-base";
 
 import styles from "./styles";
@@ -32,68 +32,68 @@ class Home extends React.Component<Props, State> {
           <Right />
         </Header>
         <Content>
-        <Swiper style={styles.wrapper}>
-        <View onResponderMove={(e) => { console.log(e); }} style={styles.slide1}>
-            <List>
-              {this.props.list.map((item, i) => (
-                <ListItem
-                  key={i}
-                  onPress={() =>
-                    this.props.navigation.navigate("BlankPage", {
-                      name: { item },
-                    })}
-                >
-                  <Text>{item}</Text>
-                </ListItem>
-              ))}
-            </List>
-          </View>
-          <View style={styles.slide2}>
-            <List>
-              {this.props.list.map((item, i) => (
-                <ListItem
-                  key={i}
-                  onPress={() =>
-                    this.props.navigation.navigate("BlankPage", {
-                      name: { item },
-                    })}
-                >
-                  <Text>{item}</Text>
-                </ListItem>
-              ))}
-            </List>
-          </View>
-          <View style={styles.slide3}>
-            <List>
-              {this.props.list.map((item, i) => (
-                <ListItem
-                  key={i}
-                  onPress={() =>
-                    this.props.navigation.navigate("BlankPage", {
-                      name: { item },
-                    })}
-                >
-                  <Text>{item}</Text>
-                </ListItem>
-              ))}
-            </List>
-          </View>
-          <View style={styles.slide4}>
-            <List>
-              {this.props.list.map((item, i) => (
-                <ListItem
-                  key={i}
-                  onPress={() =>
-                    this.props.navigation.navigate("BlankPage", {
-                      name: { item },
-                    })}
-                >
-                  <Text>{item}</Text>
-                </ListItem>
-              ))}
-            </List>
-          </View>
-        </Swiper>
+          {/* <Swiper style={styles.wrapper}>
+            <View onResponderMove={(e) => { console.log(e); }} style={styles.slide1}>
+              <List>
+                {this.props.list.map((item, i) => (
+                  <ListItem
+                    key={i}
+                    onPress={() =>
+                      this.props.navigation.navigate("BlankPage", {
+                        name: { item },
+                      })}
+                  >
+                    <Text>{item}</Text>
+                  </ListItem>
+                ))}
+              </List>
+            </View>
+            <View style={styles.slide2}>
+              <List>
+                {this.props.list.map((item, i) => (
+                  <ListItem
+                    key={i}
+                    onPress={() =>
+                      this.props.navigation.navigate("BlankPage", {
+                        name: { item },
+                      })}
+                  >
+                    <Text>{item}</Text>
+                  </ListItem>
+                ))}
+              </List>
+            </View>
+            <View style={styles.slide3}>
+              <List>
+                {this.props.list.map((item, i) => (
+                  <ListItem
+                    key={i}
+                    onPress={() =>
+                      this.props.navigation.navigate("BlankPage", {
+                        name: { item },
+                      })}
+                  >
+                    <Text>{item}</Text>
+                  </ListItem>
+                ))}
+              </List>
+            </View>
+            <View style={styles.slide4}>
+              <List>
+                {this.props.list.map((item, i) => (
+                  <ListItem
+                    key={i}
+                    onPress={() =>
+                      this.props.navigation.navigate("BlankPage", {
+                        name: { item },
+                      })}
+                  >
+                    <Text>{item}</Text>
+                  </ListItem>
+                ))}
+              </List>
+            </View>
+          </Swiper> */}
         </Content>
         <Footer style={{ backgroundColor: "#F8F8F8" }}>
           <View style={{ alignItems: "center", opacity: 0.5, flexDirection: "row" }}>
