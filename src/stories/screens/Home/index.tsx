898784e8012ea5489a19e3cc144e6d3768230a05
jaylon.qi <jaylonqi@derbysoft.com>
2017-11-08 18:18:38 +0800

    update

--- src/stories/screens/Home/index.tsx
+++ src/stories/screens/Home/index.tsx
@@ -2,9 +2,10 @@ import * as React from "react";
 import {
   View,
   Dimensions,
-  NativeModules,
-  LayoutAnimation,
+  NativeModules
+  // LayoutAnimation,
 } from "react-native";
+import { Col, Grid } from "react-native-easy-grid";
 import {
   Container,
   Header,
@@ -17,7 +18,7 @@ import {
   Body,
   Right,
   // List, ListItem,
-  Footer,
+  Footer
 } from "native-base";
 
 import styles from "./styles";
@@ -29,26 +30,21 @@ if (UIManager.setLayoutAnimationEnabledExperimental) {
 }
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
-const WIDTH_NAME = [
-  "width_1st",
-  "width_2nd",
-  "width_3rd",
-  "width_4th",
-];
+const WIDTH_NAME = ["width_1st", "width_2nd", "width_3rd", "width_4th"];
 export interface Props {
   navigation: any;
   list: any;
 }
 export interface State {}
-class Home extends React.Component < Props,
-State > {
+class Home extends React.Component<Props, State> {
   state = {
     swipeBegin: 0,
-    activeTab: 0,
+    activeTab: 1,
+    tabWidth: SCREEN_WIDTH / 10,
     [WIDTH_NAME[0]]: SCREEN_WIDTH / 10 * 7,
     [WIDTH_NAME[1]]: SCREEN_WIDTH / 10,
     [WIDTH_NAME[2]]: SCREEN_WIDTH / 10,
-    [WIDTH_NAME[3]]: SCREEN_WIDTH / 10,
+    [WIDTH_NAME[3]]: SCREEN_WIDTH / 10
   };
   constructor(props) {
     super(props);
@@ -58,7 +54,7 @@ State > {
   }
   _onSwipeBegin(swipeBegin) {
     this.setState({
-      swipeBegin: swipeBegin,
+      swipeBegin: swipeBegin
     });
   }
   _onSwipe(locationX) {
@@ -69,73 +65,87 @@ State > {
     let nextTab, activeTab;
     let nextTabWith;
     // if (Math.abs(swipeWitdh) > SCREEN_WIDTH / 4) {
-      if (swipeWitdh > 0) {
-        if (oldActiveTab + 1 > 3) {
-          nextTab = oldActiveTab + 1;
-        }
-        activeTab = oldActiveTab + 1 > 3 ? 0 : oldActiveTab + 1;
-        nextTabWith = oldActiveTab + 1 > 3 ? this.state[WIDTH_NAME[oldActiveTab + 1]] + swipeWitdh * 2 : this.state[WIDTH_NAME[oldActiveTab + 1]];
-      }else {
-        if (oldActiveTab - 1 > -1) {
-          nextTab = oldActiveTab - 1;
-        }
-        activeTab = oldActiveTab - 1 > -1 ? oldActiveTab - 1 : 4;
-        nextTabWith = oldActiveTab - 1 > -1 ? this.state[WIDTH_NAME[oldActiveTab - 1]] + swipeWitdh * 2 : this.state[WIDTH_NAME[oldActiveTab - 1]];
+    if (swipeWitdh > 0) {
+      if (oldActiveTab + 1 > 3) {
+        nextTab = oldActiveTab + 1;
       }
+      activeTab = oldActiveTab + 1 > 3 ? 0 : oldActiveTab + 1;
+      nextTabWith =
+        oldActiveTab + 1 > 3
+          ? this.state[WIDTH_NAME[oldActiveTab + 1]] + swipeWitdh * 2
+          : this.state[WIDTH_NAME[oldActiveTab + 1]];
+    } else {
+      if (oldActiveTab - 1 > -1) {
+        nextTab = oldActiveTab - 1;
+      }
+      activeTab = oldActiveTab - 1 > -1 ? oldActiveTab - 1 : 4;
+      nextTabWith =
+        oldActiveTab - 1 > -1
+          ? this.state[WIDTH_NAME[oldActiveTab - 1]] + swipeWitdh * 2
+          : this.state[WIDTH_NAME[oldActiveTab - 1]];
+    }
     // }
     this.setState({
-    activeTab,
-    // [WIDTH_NAME[oldActiveTab]]: oldActiveTabWith,
-    // [WIDTH_NAME[nextTab]]: nextTabWith,
+      activeTab
+      // [WIDTH_NAME[oldActiveTab]]: oldActiveTabWith,
+      // [WIDTH_NAME[nextTab]]: nextTabWith,
     });
   }
   _onSwipeEnd() {
     let activeTab = this.state.activeTab;
-    let newState = Object.assign({
-      [WIDTH_NAME[0]]: SCREEN_WIDTH / 10,
-      [WIDTH_NAME[1]]: SCREEN_WIDTH / 10,
-      [WIDTH_NAME[2]]: SCREEN_WIDTH / 10,
-      [WIDTH_NAME[3]]: SCREEN_WIDTH / 10,
-    }, {
-      [WIDTH_NAME[activeTab]]: SCREEN_WIDTH / 10 * 7,
-    });
+    let newState = Object.assign(
+      {
+        [WIDTH_NAME[0]]: SCREEN_WIDTH / 10,
+        [WIDTH_NAME[1]]: SCREEN_WIDTH / 10,
+        [WIDTH_NAME[2]]: SCREEN_WIDTH / 10,
+        [WIDTH_NAME[3]]: SCREEN_WIDTH / 10
+      },
+      {
+        [WIDTH_NAME[activeTab]]: SCREEN_WIDTH / 10 * 7
+      }
+    );
     // Animate the update
-    LayoutAnimation.easeInEaseOut(LayoutAnimation.Presets.easeInEaseOut, () => {
-      console.log("anitmation end.");
-    });
+    // LayoutAnimation.easeInEaseOut(LayoutAnimation.Presets.easeInEaseOut, () => {
+    //   console.log("anitmation end.");
+    // });
     this.setState(newState);
   }
   render() {
-    return (
-      <Container style={styles.container}>
+    return <Container style={styles.container}>
         <Header>
           <Left>
             <Button transparent>
-              <Icon
-                active
-                name="menu"
-                onPress={() => this.props.navigation.navigate("DrawerOpen")}/>
+              <Icon active name="menu" onPress={() => this.props.navigation.navigate("DrawerOpen")} />
             </Button>
           </Left>
           <Body>
             <Title>D O</Title>
           </Body>
-          <Right/>
+          <Right />
         </Header>
-        <View
-        style={{flexDirection: "row", flex: 1, width: "100%", height: "100%"}}
-          onStartShouldSetResponder={(evt) => {
+        <View style={{ flexDirection: "row", flex: 1, width: "100%", height: "100%" }} onStartShouldSetResponder={evt => {
             this._onSwipeBegin(evt.nativeEvent.locationX);
             return true;
-          }}
-          onResponderMove={(evt) => {
+          }} onResponderMove={evt => {
             this._onSwipe(evt.nativeEvent.locationX);
-          }}
-          onResponderRelease={() => {
+          }} onResponderRelease={() => {
             this._onSwipeEnd();
-          }}
-          >
-          <View style={{width: this.state[WIDTH_NAME[0]], height: "100%", backgroundColor: "#f1f"}}>
+          }}>
+          <Grid>
+            <Col style={[styles.tab1, this.state.activeTab === 1 ? {} : styles.tab]}>
+              <Text>Fixed width</Text>
+            </Col>
+            <Col style={[styles.tab2, this.state.activeTab === 2 ? {} : styles.tab]}>
+              <Text>Fluid width</Text>
+            </Col>
+            <Col style={[styles.tab3, this.state.activeTab === 3 ? {} : styles.tab]}>
+              <Text>Fluid width</Text>
+            </Col>
+            <Col style={[styles.tab4, this.state.activeTab === 4 ? {} : styles.tab]}>
+              <Text>Fluid width</Text>
+            </Col>
+          </Grid>
+          {/* <View style={{width: this.state[WIDTH_NAME[0]], height: "100%", backgroundColor: "#f1f"}}>
           <Text>1st</Text>
           </View>
           <View style={{width: this.state[WIDTH_NAME[1]], height: "100%", backgroundColor: "#ddd"}}>
@@ -146,27 +156,18 @@ State > {
           </View>
           <View style={{width: this.state[WIDTH_NAME[3]], height: "100%", backgroundColor: "#fe1"}}>
           <Text>4th</Text>
-          </View>
+          </View> */}
         </View>
-        <Footer style={{
-          backgroundColor: "#F8F8F8",
-        }}>
-          <View
-            style={{
-            alignItems: "center",
-            opacity: 0.5,
-            flexDirection: "row",
-          }}>
+        <Footer style={{ backgroundColor: "#F8F8F8" }}>
+          <View style={{ alignItems: "center", opacity: 0.5, flexDirection: "row" }}>
             {/* <View padder> */}
-            <Text style={{
-              color: "#ec4c4c",
-            }}>Have none account?
+            <Text style={{ color: "#ec4c4c" }}>
+              Have none account?
             </Text>
             {/* </View> */}
           </View>
         </Footer>
-      </Container>
-    );
+      </Container>;
   }
 }
 
--- src/stories/screens/Home/styles.tsx
+++ src/stories/screens/Home/styles.tsx
@@ -1,22 +1,25 @@
-import { StyleSheet } from "react-native";
-
+import { StyleSheet, Dimensions } from "react-native";
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const TAB_WIDTH = SCREEN_WIDTH / 10;
 const styles: any = StyleSheet.create({
 	container: {
 		// flexDirection: "row",
 		backgroundColor: "#FBFAFA",
 	},
-	row: {
-		flex: 1,
-		alignItems: "center",
+	tab: {
+		width: TAB_WIDTH
 	},
-	text: {
-		fontSize: 20,
-		marginBottom: 15,
-		alignItems: "center",
+	tab1: {
+		backgroundColor: "#f1f"
 	},
-	mt: {
-		marginTop: 18,
+	tab2: {
+		backgroundColor: "#ddd"
 	},
-	wrapper: {},
+	tab3: {
+		backgroundColor: "#fff"
+	},
+	tab4: {
+		backgroundColor: "#fe1"
+	},
 });
 export default styles;
