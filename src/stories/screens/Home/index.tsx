f1f9ead90b3d591fa9ff266151d93b828fc51900
JaylonQi@outlook.com <JaylonQi@outlook.com>
2017-12-27 19:01:17 +0800

    native-base

--- src/stories/screens/Home/index.tsx
+++ src/stories/screens/Home/index.tsx
@@ -46,8 +46,8 @@ class Home extends React.Component<Props, State> {
     [WIDTH_NAME[2]]: SCREEN_WIDTH / 10,
     [WIDTH_NAME[3]]: SCREEN_WIDTH / 10
   };
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this._onSwipe = this._onSwipe.bind(this);
     this._onSwipeBegin = this._onSwipeBegin.bind(this);
     this._onSwipeEnd = this._onSwipeEnd.bind(this);
@@ -59,56 +59,56 @@ class Home extends React.Component<Props, State> {
   }
   _onSwipe(locationX) {
     console.log(locationX, SCREEN_WIDTH);
-    let swipeWitdh = locationX - this.state.swipeBegin;
-    let oldActiveTab = this.state.activeTab;
-    // let oldActiveTabWith = this.state[WIDTH_NAME[oldActiveTab]] - swipeWitdh * 2;
-    let nextTab, activeTab;
-    let nextTabWith;
-    // if (Math.abs(swipeWitdh) > SCREEN_WIDTH / 4) {
-    if (swipeWitdh > 0) {
-      if (oldActiveTab + 1 > 3) {
-        nextTab = oldActiveTab + 1;
-      }
-      activeTab = oldActiveTab + 1 > 3 ? 0 : oldActiveTab + 1;
-      nextTabWith =
-        oldActiveTab + 1 > 3
-          ? this.state[WIDTH_NAME[oldActiveTab + 1]] + swipeWitdh * 2
-          : this.state[WIDTH_NAME[oldActiveTab + 1]];
-    } else {
-      if (oldActiveTab - 1 > -1) {
-        nextTab = oldActiveTab - 1;
-      }
-      activeTab = oldActiveTab - 1 > -1 ? oldActiveTab - 1 : 4;
-      nextTabWith =
-        oldActiveTab - 1 > -1
-          ? this.state[WIDTH_NAME[oldActiveTab - 1]] + swipeWitdh * 2
-          : this.state[WIDTH_NAME[oldActiveTab - 1]];
-    }
+    // let swipeWitdh = locationX - this.state.swipeBegin;
+    // let oldActiveTab = this.state.activeTab;
+    // // let oldActiveTabWith = this.state[WIDTH_NAME[oldActiveTab]] - swipeWitdh * 2;
+    // let nextTab, activeTab;
+    // let nextTabWith;
+    // // if (Math.abs(swipeWitdh) > SCREEN_WIDTH / 4) {
+    // if (swipeWitdh > 0) {
+    //   if (oldActiveTab + 1 > 3) {
+    //     nextTab = oldActiveTab + 1;
+    //   }
+    //   activeTab = oldActiveTab + 1 > 3 ? 0 : oldActiveTab + 1;
+    //   nextTabWith =
+    //     oldActiveTab + 1 > 3
+    //       ? this.state[WIDTH_NAME[oldActiveTab + 1]] + swipeWitdh * 2
+    //       : this.state[WIDTH_NAME[oldActiveTab + 1]];
+    // } else {
+    //   if (oldActiveTab - 1 > -1) {
+    //     nextTab = oldActiveTab - 1;
+    //   }
+    //   activeTab = oldActiveTab - 1 > -1 ? oldActiveTab - 1 : 4;
+    //   nextTabWith =
+    //     oldActiveTab - 1 > -1
+    //       ? this.state[WIDTH_NAME[oldActiveTab - 1]] + swipeWitdh * 2
+    //       : this.state[WIDTH_NAME[oldActiveTab - 1]];
     // }
-    this.setState({
-      activeTab
+    // }
+    // this.setState({
+    //   activeTab
       // [WIDTH_NAME[oldActiveTab]]: oldActiveTabWith,
       // [WIDTH_NAME[nextTab]]: nextTabWith,
-    });
+    // });
   }
   _onSwipeEnd() {
-    let activeTab = this.state.activeTab;
-    let newState = Object.assign(
-      {
-        [WIDTH_NAME[0]]: SCREEN_WIDTH / 10,
-        [WIDTH_NAME[1]]: SCREEN_WIDTH / 10,
-        [WIDTH_NAME[2]]: SCREEN_WIDTH / 10,
-        [WIDTH_NAME[3]]: SCREEN_WIDTH / 10
-      },
-      {
-        [WIDTH_NAME[activeTab]]: SCREEN_WIDTH / 10 * 7
-      }
-    );
+    // let activeTab = this.state.activeTab;
+    // let newState = Object.assign(
+    //   {
+    //     [WIDTH_NAME[0]]: SCREEN_WIDTH / 10,
+    //     [WIDTH_NAME[1]]: SCREEN_WIDTH / 10,
+    //     [WIDTH_NAME[2]]: SCREEN_WIDTH / 10,
+    //     [WIDTH_NAME[3]]: SCREEN_WIDTH / 10
+    //   },
+    //   {
+    //     [WIDTH_NAME[activeTab]]: SCREEN_WIDTH / 10 * 7
+    //   }
+    // );
     // Animate the update
     // LayoutAnimation.easeInEaseOut(LayoutAnimation.Presets.easeInEaseOut, () => {
     //   console.log("anitmation end.");
     // });
-    this.setState(newState);
+    // this.setState(newState);
   }
   render() {
     return <Container style={styles.container}>
@@ -125,38 +125,26 @@ class Home extends React.Component<Props, State> {
         </Header>
         <View style={{ flexDirection: "row", flex: 1, width: "100%", height: "100%" }} onStartShouldSetResponder={evt => {
             this._onSwipeBegin(evt.nativeEvent.locationX);
-            return true;
+            {/*return true;*/}
           }} onResponderMove={evt => {
             this._onSwipe(evt.nativeEvent.locationX);
           }} onResponderRelease={() => {
             this._onSwipeEnd();
           }}>
           <Grid>
-            <Col style={[styles.tab1, this.state.activeTab === 1 ? {} : styles.tab]}>
+            <Col style={{width: this.state[WIDTH_NAME[0]], height: "100%"}}>
               <Text>Fixed width</Text>
             </Col>
-            <Col style={[styles.tab2, this.state.activeTab === 2 ? {} : styles.tab]}>
+            <Col style={{width: this.state[WIDTH_NAME[1]], height: "100%", backgroundColor: "#ddd"}}>
               <Text>Fluid width</Text>
             </Col>
-            <Col style={[styles.tab3, this.state.activeTab === 3 ? {} : styles.tab]}>
+            <Col style={{width: this.state[WIDTH_NAME[2]], height: "100%", backgroundColor: "#fff"}}>
               <Text>Fluid width</Text>
             </Col>
-            <Col style={[styles.tab4, this.state.activeTab === 4 ? {} : styles.tab]}>
+            <Col style={{width: this.state[WIDTH_NAME[3]], height: "100%", backgroundColor: "#fe1"}}>
               <Text>Fluid width</Text>
             </Col>
           </Grid>
-          {/* <View style={{width: this.state[WIDTH_NAME[0]], height: "100%", backgroundColor: "#f1f"}}>
-          <Text>1st</Text>
-          </View>
-          <View style={{width: this.state[WIDTH_NAME[1]], height: "100%", backgroundColor: "#ddd"}}>
-          <Text>2nd</Text>
-          </View>
-          <View style={{width: this.state[WIDTH_NAME[2]], height: "100%", backgroundColor: "#fff"}}>
-          <Text>3rd</Text>
-          </View>
-          <View style={{width: this.state[WIDTH_NAME[3]], height: "100%", backgroundColor: "#fe1"}}>
-          <Text>4th</Text>
-          </View> */}
         </View>
         <Footer style={{ backgroundColor: "#F8F8F8" }}>
           <View style={{ alignItems: "center", opacity: 0.5, flexDirection: "row" }}>
