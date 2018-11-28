68c352c3dcf2985f635ed947eade540047006c00
arvin.qi <arvin.qi@qq.com>
2018-11-28 13:32:35 +0000

    prisma & theme

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,28 +1,40 @@
 import App, { Container } from 'next/app';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from 'styled-components';
 import { ApolloProvider } from 'react-apollo';
 import withData from '../lib/withData';
+import theme from '../lib/theme';
+
+// material theme
+const MuiTheme = createMuiTheme({
+    palette: theme
+});
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        // this exposes the query to the user
+        pageProps.query = ctx.query;
+        return { pageProps };
     }
-    // this exposes the query to the user
-    pageProps.query = ctx.query;
-    return { pageProps };
-  }
-  render() {
-    const { Component, apollo, pageProps } = this.props;
+    render() {
+        const { Component, apollo, pageProps } = this.props;
 
-    return (
-      <Container>
-        <ApolloProvider client={apollo}>
-            <Component {...pageProps} />
-        </ApolloProvider>
-      </Container>
-    );
-  }
+        return (
+            <Container>
+                <ApolloProvider client={apollo}>
+                    <ThemeProvider theme={theme}>
+                        <MuiThemeProvider theme={MuiTheme}>
+                            <Component {...pageProps} />
+                        </MuiThemeProvider>
+                    </ThemeProvider>
+                </ApolloProvider>
+            </Container>
+        );
+    }
 }
 
 export default withData(MyApp);
